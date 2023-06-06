35b50aff98a19e462c7e3214bcc5c9c458bd2237
William "Bill" Overbeck <135356356+TestieTendie@users.noreply.github.com>
2023-06-05 23:12:36 -0600

    Update l4d2-server-backup.sh

--- l4d2-server-backup.sh
+++ l4d2-server-backup.sh
@@ -1,15 +1,12 @@
-!/bin/bash
-# Ideally, your provider should allow you to authenticate to SFTP using an SSH key, that way the process can be entirely automated,
-# otherwise you need to input your SFTP password each time a new SFTP session is started (14 times in this script).
+#!/bin/bash
 
 # SFTP Connection details
 
-SERVER="PUT YOUR SERVER'S SFTP ADDRESS HERE"
-PORT="2022"
-USERNAME="PUT YOUR SFTP USER HERE"
+SERVER="SERVER'S SFTP ADDRESS HERE"
+PORT="SERVER'S SFTP PORT HERE"
+USERNAME="YOUR USERNAME TO ACCESS THE SERVER VIA SFTP HERE"
 
 # Define the directories and files to fetch
-# Ideally, a sourcemode plugin would make the backups to a single folder and we'd fetch that but I haven't wrote said plugin yet.
 DIRECTORIES=(
   "/left4dead2/cfg/"
   "/left4dead2/ems/"
@@ -34,25 +31,43 @@ LOCAL_DIR="./l4d2-server-backup"
 
 # Copy Directories
 for dir in "${DIRECTORIES[@]}"; do
-  target_dir="$LOCAL_DIR${dir%/}"
+  target_dir="$LOCAL_DIR${dir%/}"  # Remove trailing slash if present
   mkdir -p "$target_dir"
-  sftp -P "$PORT" "$USERNAME@$SERVER" << EOF
-    lcd "$target_dir"
-    cd "$dir"
-    get -r *
-    bye
+  expect << EOF
+    set timeout -1
+    spawn sftp -P $PORT $USERNAME@$SERVER
+    expect "USERNAME@SERVER's password:"
+    send "YOUR PASSWORD HERE\r"
+    expect "sftp>"
+    send "lcd \"$target_dir\"\r"
+    expect "sftp>"
+    send "cd \"$dir\"\r"
+    expect "sftp>"
+    send "get -r *\r"
+    expect "sftp>"
+    send "bye\r"
+    expect eof
 EOF
 done
 
 # Copy Files
 for file in "${FILES[@]}"; do
-  sftp -P $PORT $USERNAME@$SERVER << EOF
-    get $file $LOCAL_DIR$file
+  target_file="$LOCAL_DIR$file"
+  expect << EOF
+    set timeout -1
+    spawn sftp -P $PORT $USERNAME@$SERVER
+    expect "USERNAME@SERVER's password:"
+    send "YOUR PASSWORD HERE\r"
+    expect "sftp>"
+    send "bye\r"
+    expect eof
 EOF
 done
 
-# Commit and push files to git repo
+# Commit and push files to the git repo with default commit message
 cd ~/l4d2-server-backup
 git add .
 git commit -m "Fetched files from server"
 git push origin main
+
+rm -rf ~/l4d2-server-backup/left4dead2
